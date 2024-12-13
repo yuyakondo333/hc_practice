c1bd29b6fefeb0ca2085e7b319c558d811a65c96
yuyakondo <yuyakondo333@gmail.com>
2024-12-14 01:19:08 +0900

    get_buyable_juiceメソッドとコメントを修正

--- a.py
+++ a.py
@@ -2,13 +2,13 @@ class Suica:
     def __init__(self, deposit=500):
         self.__deposit = deposit
 
-    # 預かり金残高を取得する getter
     @property
+    # 預かり金残高を取得する getter
     def deposit(self):
         return self.__deposit
 
-    # 残高をチャージする setter
     @deposit.setter
+    # 残高をチャージする setter
     def deposit(self, amount):
         self.__deposit += amount
 
